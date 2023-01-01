eea041305e86cad37920d8ecf481b970c45650f0
garid3000 <garidzorigoo@gmail.com>
2023-01-01 19:24:40 +0900

    Made it accept JPEG & jpg files, & fixed R channel (prev. forget R with G)

--- main.go
+++ main.go
@@ -6,12 +6,15 @@ import (
 	"image"
 	"image/color"
 	"image/png"
+	"image/jpeg"
 	"log"
+	"strings"
 )
 
 const height   int = 1080
 const width    int = 1920
 const channel  int = 4
+
 var (
 	fb [height * width * channel] byte
 )
@@ -22,9 +25,9 @@ func ShowImgOnFrambeBuffer(img image.Image){
 		for x:=img.Bounds().Min.X; x<img.Bounds().Max.X; x++ {
 			tmp := img.At(x,y)
 			col := color.RGBAModel.Convert(tmp).(color.RGBA)
-			fb[y * width * channel + x * channel    ] = col.B
+			fb[y * width * channel + x * channel    ] = col.B 
 			fb[y * width * channel + x * channel + 1] = col.G
-			fb[y * width * channel + x * channel + 2] = col.G
+			fb[y * width * channel + x * channel + 2] = col.R
 			fb[y * width * channel + x * channel + 3] = col.A
 		}
 	}
@@ -36,8 +39,8 @@ func ShowImgOnFrambeBuffer(img image.Image){
 	}
 }
 
-// ShowPNG_onFB ...
-func ShowPNG_onFB(fname string) {
+
+func fname2fb(fname string) {
 	imgfile, err := os.Open(fname)
 	if err != nil {
 		log.Fatal(err)
@@ -45,7 +48,22 @@ func ShowPNG_onFB(fname string) {
 	defer imgfile.Close()
 
 
-	img, err := png.Decode(imgfile)
+	var img image.Image;
+	fnameParts := strings.Split(fname, ".")  //splits fname with dot . 
+	extStr     := strings.ToLower(
+		fnameParts[len(fnameParts)-1])       //last item from slice,& make it lowercase
+
+
+	switch (extStr) {
+	case "png":
+		img, err = png.Decode(imgfile)
+	case "jpeg", "jpg":
+		img, err = jpeg.Decode(imgfile)
+	default:
+		panic(123)
+	} 
+	
+
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -54,7 +72,8 @@ func ShowPNG_onFB(fname string) {
 
 
 
+
 func main(){
 	fname := os.Args[1]   // for the command line arguments
-	ShowPNG_onFB(fname)
+	fname2fb(fname)
 }
